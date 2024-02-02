26686d772a0d0853b91d246ecce070bc4d7ca5b9
Marek Vondra <marek.vondra@t-systems.com>
2024-02-02 15:57:11 +0100

    init commit

---
+++ src/main/java/medium/learn/metric/metricproject/MetricProjectApplication.java
@@ -0,0 +1,13 @@
+package medium.learn.metric.metricproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MetricProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/medium/learn/metric/metricproject/MetricProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package medium.learn.metric.metricproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
